5db555951868f4c15408d247b48d748dc72cf2cc
hohlov-d <hohlov_d.o@mail.ru>
2024-06-20 14:22:06 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def devide (first,second):
+    if second != 0:
+        return print(first / second)
+    else:
+        return print('Ошибка')
---
+++ true_math.py
@@ -0,0 +1,6 @@
+import math
+def devide (first,second):
+    if second != 0:
+        return print(first / second)
+    else:
+        return print(math.inf)
